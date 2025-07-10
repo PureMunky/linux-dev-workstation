a909b9080902683d0a70caba19dc45e89256ba01
Phil Corbett <corbett.phil@gmail.com>
2025-07-10 12:21:41 -0400

    Add user details and readme.

---
+++ readme.md
@@ -0,0 +1,11 @@
+# My Linux Workstation Setup
+
+To enable the execution of shell scripts in this repository, you can run the following command:
+```bash
+find . -name "*.sh" -exec chmod +x {} \;
+```
+
+Then to run the setup script, execute:
+```bash
+./setup.sh
+```
--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -5,8 +5,8 @@
 set -e
 
 echo "[+] Configuring Git..."
-git config --global user.name "Your Name"
-git config --global user.email "your.email@example.com"
+git config --global user.name "Phil Corbett"
+git config --global user.email "corbett.phil@gmail.com"
 git config --global core.editor "vim"
 git config --global init.defaultBranch main
 
